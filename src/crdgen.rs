48b51fbbb3480bcdef0a29f39168b2ea65112e6a
june <dev@bug.jp.net>
2026-04-27 11:05:43 +0000

    added helm.sh/resource-policy annotation to crdgen

--- src/crdgen.rs
+++ src/crdgen.rs
@@ -1,8 +1,12 @@
 use kube::CustomResourceExt;
 use rauthy_controller::controller;
+
 fn main() {
-    print!(
-        "{}",
-        serde_yaml::to_string(&controller::OIDCClient::crd()).unwrap()
-    );
+    let mut crd = controller::OIDCClient::crd();
+    crd.metadata
+        .annotations
+        .get_or_insert_with(Default::default)
+        .insert("helm.sh/resource-policy".to_string(), "keep".to_string());
+
+    print!("{}", serde_yaml::to_string(&crd).unwrap());
 }
